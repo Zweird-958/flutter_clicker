f856e680bffb114c2ec112e2685673d6ca2b1313
Zweird-958 <xdfamilyytt@gmail.com>
2023-02-24 12:09:18 +0100

    State and Icon Color Price

--- lib/pages/Shop.dart
+++ lib/pages/Shop.dart
@@ -1,48 +1,58 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class Shop extends StatefulWidget {
-  const Shop({super.key});
+  List<Map<String, dynamic>> items;
+  Function buyItem;
+  int money;
+  Shop({
+    super.key,
+    required this.items,
+    required this.buyItem,
+    required this.money,
+  });
 
   @override
   State<Shop> createState() => _ShopState();
 }
 
 class _ShopState extends State<Shop> {
-  final items = [
-    {'title': 'Miner', 'price': 10, 'cpc': 1, 'cps': 0, 'owned': 0},
-    {'title': 'Miner', 'price': 10, 'cpc': 1, 'cps': 0, 'owned': 0},
-    {'title': 'Miner', 'price': 10, 'cpc': 1, 'cps': 0, 'owned': 0},
-  ];
-
-  // RECUPERER LITEM CORRESPOND GRACE A LINDEX
-
+  // ignore: non_constant_identifier_names
   Widget TextWithPadding(label) => Padding(
-        padding: EdgeInsets.only(left: 2.5, right: 2.5),
+        padding: const EdgeInsets.only(left: 2.5, right: 2.5),
         child: Text(label),
       );
 
   @override
   Widget build(BuildContext context) {
+    List<Map<String, dynamic>> items = widget.items;
+
     return Center(
       child: ListView.builder(
         itemCount: items.length,
         itemBuilder: (context, index) {
           final item = items[index];
           final title = item["title"];
+          int price = item["price"];
 
           return ListTile(
             title: Text("$title"),
             subtitle: Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                TextWithPadding("Price : ${item["price"]}\$"),
+                TextWithPadding("Price : $price\$"),
                 TextWithPadding("CPC : ${item["cpc"]}"),
                 TextWithPadding("CPS : ${item["cps"]}"),
                 TextWithPadding("Owned : x${item["owned"]}"),
               ],
+            ),
+            trailing: IconButton(
+              onPressed: widget.buyItem(index),
+              icon: Icon(
+                Icons.monetization_on,
+                color: widget.money < price ? Colors.red : Colors.green,
+              ),
             ),
           );
         },
