3fb22e61824ac3e395b5f71e75d2357da0d8c8ab
Zweird-958 <xdfamilyytt@gmail.com>
2023-02-26 11:24:48 +0100

    Delete test Calendar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_clicker_2/pages/Calendar.dart';
 import 'package:flutter_clicker_2/pages/Home.dart';
 import 'package:flutter_clicker_2/pages/Shop.dart';
 import 'package:flutter_clicker_2/pages/Stats.dart';
